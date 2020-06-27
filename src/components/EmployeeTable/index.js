11397c18b928f01db326027b8d14ae7ff76075f6
ccrum292 <ccrum292@gmail.com>
2020-06-27 11:32:08 -0400

    added filterEmployeesBasedOnSearch function

--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -4,14 +4,53 @@ import axios from "axios";
 
 function EmployeeTable(props){
   let employeeStorage = [];
-  const [displayedEmployees, setDisplayedEmployess] = useState();
+  const [displayedEmployees, setDisplayedEmployess] = useState([{
+    "name": {
+    "title": "Mrs",
+    "first": "Beatrice",
+    "last": "Coleman"
+    },
+    "email": "beatrice.coleman@example.com",
+    "dob": {
+    "date": "1958-11-19T07:20:18.851Z",
+    "age": 62
+    },
+    "phone": "(025)-901-5259",
+    "picture": {
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/women/54.jpg"
+    }
+  }]);
+
   useEffect(() => {
-    console.log("hello")
     axios.get("https://randomuser.me/api/?results=200&nat=us").then(data => {
       employeeStorage = data.data.results;
       setDisplayedEmployess(employeeStorage);
     });
   },[]);
+
+  useEffect(() => {
+    console.log("search change")
+    // setDisplayedEmployess(filterEmployeesBasedOnSearch(props));
+    // console.log(displayedEmployees);
+    console.log(filterEmployeesBasedOnSearch(props))
+  },[props.state.search]);
+
+  const filterEmployeesBasedOnSearch = (props) => {
+    let currentSearchArray = props.state.search.toLocaleLowerCase().split("");
+    if(!currentSearchArray[0]){
+      console.log("nothing in search")
+      return displayedEmployees
+    }
+    const newArrayOfEmployeesBasedOnSearch = displayedEmployees.filter(val => {
+      const fullnameForSearchArray = (val.name.first.toLocaleLowerCase() + " " + val.name.last.toLocaleLowerCase()).split("");
+      const comparisonArray = [];
+      for(let i = 0; i<currentSearchArray.length; i++){
+        comparisonArray.push(fullnameForSearchArray[i]);
+      }
+      return comparisonArray.join("") === currentSearchArray.join("");
+    });
+    return newArrayOfEmployeesBasedOnSearch;
+  };
 
   const compareNamesA = (a,b) => {
     let comparison = 0;
@@ -93,32 +132,36 @@ function EmployeeTable(props){
   };
 
   return(
+    <div>
+      <div>{JSON.stringify(displayedEmployees[0])}</div>
       <Table striped bordered hover variant={props.variant}>
-      <thead>
-        <th>Image</th>
-        <th onClick={sortByName}>Name</th>
-        <th onClick={sortByPhone}>Phone</th>
-        <th onClick={sortByEmail}>Email</th>
-        <th onClick={sortByDOB}>DOB</th>
-      </thead>
-      <tbody>
-        {displayedEmployees ? displayedEmployees.map((data) => (
-          <tr>
-            <td className="d-flex justify-content-center"><img alt="Employee Headshot" src={data.picture.thumbnail}></img></td>
-            <td>{data.name.first} {data.name.last}</td>
-            <td>{data.phone}</td>
-            <td>{data.email}</td>
-            <td>{data.dob.date}</td>
-          </tr>
-        )): (<tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>)}
-      </tbody>
-    </Table>
+        <thead>
+          <th>Image</th>
+          <th onClick={sortByName}>Name</th>
+          <th onClick={sortByPhone}>Phone</th>
+          <th onClick={sortByEmail}>Email</th>
+          <th onClick={sortByDOB}>DOB</th>
+        </thead>
+        <tbody>
+          {displayedEmployees ? displayedEmployees.map((data) => (
+            <tr>
+              <td className="d-flex justify-content-center"><img alt="Employee Headshot" src={data.picture.thumbnail}></img></td>
+              <td>{data.name.first} {data.name.last}</td>
+              <td>{data.phone}</td>
+              <td>{data.email}</td>
+              <td>{data.dob.date}</td>
+            </tr>
+          )): (<tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>)}
+        </tbody>
+      </Table>
+
+    </div>
   );
 };
 
